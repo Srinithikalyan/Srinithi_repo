e4d9cf9aa6a72446070c211fb956567b6ac67d89
Srinithi Kalyanaraman <kalyansrinithi@gmail.com>
2017-09-09 15:27:44 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+print "**********Basic Program*******"
+print "Enter two numbers for processing"
+a = int(raw_input())
+b = int(raw_input())
+c = 0
+c = a + b
+print "The Sum of a and b is ", c
+c = a - b
+print "The difference between a and b is ", c
+c = a * b
+print "The product of a and b is ", c
+c = a / b
+print "The division of a and b is ", c
+c = a % b
+print "The modulo of a and b is ", c
+c = a ** b
+print "The exponent result of a and b is ", c
+
