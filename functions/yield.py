d53e110b1ef2c8976f77e642c01bbe88862d1bcc
Srinithikalyan <kalyansrinithi@gmail.com>
2017-10-25 12:23:20 +0530

    Yield and return

--- functions/yield.py
+++ functions/yield.py
@@ -1,1 +1,20 @@
 print "Yield and Return statements"
+def func1():
+    a = 0
+    while True:
+        a += 1
+    	return a
+def func2():
+    a = 0
+    while True:
+        a += 1
+    	yield a
+     
+f1 = func1()
+f2 = func2()
+for i in range(5):
+    print f1
+for i in range(5):
+    print next(f2)
+
+
