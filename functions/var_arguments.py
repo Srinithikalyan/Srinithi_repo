8f3dc602b90b4f5cab290c9d8e6c1121e951e4ea
Srinithikalyan <kalyansrinithi@gmail.com>
2017-10-25 14:43:22 +0530

    args and kwargs

---
+++ functions/var_arguments.py
@@ -0,0 +1,22 @@
+print "*******Variable arguments *args and **kwargs *******"
+
+
+def multiply(*args):        #args function definition
+    for i in args:
+        print "%i x 12 = %i" % (i,(i*12))
+
+
+def sum(a,b):               #kwargs function definition
+    sum = 0
+    sum = a + b
+    print "%i + %i = %i" % (a,b,sum)
+
+
+print "\nThe multiplication table of 12 is"
+multiply(*range(1,101))     #args function call
+
+
+print "\n\nThe Addition result is"
+kwargs = {'a':7,'b':3}      
+sum(**kwargs)               #kwargs function call
+
