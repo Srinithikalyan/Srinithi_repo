356ad40ed3727691206e595c51f2590430c5c4c6
Srinithikalyan <kalyansrinithi@gmail.com>
2017-10-25 17:08:18 +0530

    After review changes

--- loops/vow_con.py
+++ loops/vow_con.py
@@ -1,11 +1,13 @@
 print "***********Vowels and consonants**********"
+
+
 str = str(raw_input("\n Enter the string to find vowels and consonants "))
 vow = "aeiou"
 
 
-for i in str:
-    if i in vow:
-        print "\n\"%s\" is Vowel in the word \"%s\"\n " % (i,str)
+for iter in str:
+    if iter in vow:
+        print "\n\"%s\" is Vowel in the word \"%s\"\n " % (iter,str)
     else:
-        print "\n\"%s\" is Consonant in the word \"%s\"\n " % (i,str)
+        print "\n\"%s\" is Consonant in the word \"%s\"\n " % (iter,str)
 
