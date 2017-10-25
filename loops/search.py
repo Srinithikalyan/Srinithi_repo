49f49461ec8181fb25e34e7d0dca5b7a1283f56d
Srinithikalyan <kalyansrinithi@gmail.com>
2017-10-25 17:14:41 +0530

    After review changes

--- loops/search.py
+++ loops/search.py
@@ -1,15 +1,20 @@
 print "****Searching for substring*******"
 string = str(raw_input("\n Enter the string "))
 sub = str(raw_input("\n Enter the substring to be searched "))
-res = sub in string
-if res == True:
+
+
+if sub in string:
     print "\n The entered substring \'%s\' is present in string \'%s\'" % (sub,string)
 else:
     print "\n The entered substring is not present"
+
+
 print "******Reversing a string******"
 print string[::-1]
+
+
 print "*****Printing characters from string*****"
-for i in range(len(string)):
-    print string[i]
+for iter in string:
+    print iter
 
 
