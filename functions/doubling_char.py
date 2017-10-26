9c1598f2f958cf75efa8b9d9c1a4da83f90fe526
Srinithikalyan <kalyansrinithi@gmail.com>
2017-10-26 12:06:52 +0530

    Functions

---
+++ functions/doubling_char.py
@@ -0,0 +1,12 @@
+from __future__ import print_function    #importing print() from python 3
+
+print("*******Doubling each characters in string*******")
+
+
+def double_char(char):                   #function definition
+    for i in char: print(i * 2, end = "")
+
+
+char = raw_input("\nEnter the string to double its character: ")
+print("\nThe resultant string is ", end = "")
+double_char(char)                       #function call
